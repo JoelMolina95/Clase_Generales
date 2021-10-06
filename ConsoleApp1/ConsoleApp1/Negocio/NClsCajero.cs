6ae9691e58ecb326c75bf18f9f0cba2aa61180dc
JoelJR26 <91918009+JoelMolina95@users.noreply.github.com>
2021-10-05 20:51:21 -0600

    Cajero

---
+++ ConsoleApp1/ConsoleApp1/Negocio/NClsCajero.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleApp1.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Negocio
+{
+    class NClsCajero
+    {
+
+        public String SacarDinero(ClsCajero constructor)
+        {
+
+            if (constructor.Dato % 5 == 0)
+
+            {
+
+                return " Puedes sacar tu dinero";
+            }
+
+            else
+            {
+                return " No puedes retirar tu dinero ";
+            }
+        }
+    }
+}
+    
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleApp1.Entidades;
+using ConsoleApp1.Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ClsCajero constructor = new ClsCajero();
+            NClsCajero nclscajero = new NClsCajero();
+
+
+            int opcion = 1;
+            int dato;
+            while (opcion == 1)
+            {
+
+                Console.WriteLine(" Ingrese la cantidad que deseas retirar");
+                dato = Convert.ToInt32(Console.ReadLine());
+                constructor.Dato = dato;
+                Console.WriteLine(nclscajero.SacarDinero(constructor));
+            }
+        }
+    }
+}
+
+        
